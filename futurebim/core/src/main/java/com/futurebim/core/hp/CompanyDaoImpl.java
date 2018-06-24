8f79d70e25b8fd4561ad87138683576387692b09
hamid <hamidrezaseifi@gmail.com>
2018-06-24 14:36:23 +0200

    renames

---
+++ futurebim/core/src/main/java/com/futurebim/core/hp/CompanyDaoImpl.java
@@ -0,0 +1,67 @@
+package com.futurebim.core.hp;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Repository
+public class CompanyDaoImpl implements CompanyDao {
+
+	private static final Logger logger = LoggerFactory.getLogger(CompanyDaoImpl.class);
+	
+	@Autowired
+  private SessionFactory sessionFactory;
+	
+	
+	@Override
+	public void addCompany(Company c) {
+		Session session = this.sessionFactory.getCurrentSession();
+		session.persist(c);
+		logger.info("Company saved successfully, Company Details=" + c);
+	}
+
+	@Override
+	public void updateCompany(Company c) {
+		Session session = this.sessionFactory.getCurrentSession();
+		session.update(c);
+		logger.info("Company updated successfully, Company Details=" + c);
+
+	}
+
+	@Override
+	public void removeCompany(int id) {
+		Session session = this.sessionFactory.getCurrentSession();
+		Company c = (Company) session.load(Company.class, new Integer(id));
+		if(null != c){
+			session.delete(c);
+		}
+		logger.info("Company deleted successfully, person details="+c);
+
+	}
+
+	@Override
+	public Company getById(int id) {
+		Session session = this.sessionFactory.getCurrentSession();		
+		Company c = (Company) session.load(Company.class, new Integer(id));
+		logger.info("Company loaded successfully, Company details="+c);
+		return c;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Company> listCountries() {
+		Session session = this.sessionFactory.getCurrentSession();
+		List<Company> personsList = session.createQuery("from Company").list();
+		for(Company c : personsList){
+			logger.info("Company List::"+c);
+		}
+		return personsList;
+	}
+
+}
---
+++ futurebim/gui/src/main/java/com/futurebim/gui/model/AddressPK.java
@@ -0,0 +1,52 @@
+package com.futurebim.gui.model;
+
+import java.io.Serializable;
+
+/**
+ * The primary key class for the addresses database table.
+ * 
+ */
+public class AddressPK implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int ownerid;
+
+	private String ownerType;
+
+	public AddressPK() {
+	}
+	public int getOwnerid() {
+		return this.ownerid;
+	}
+	public void setOwnerid(int ownerid) {
+		this.ownerid = ownerid;
+	}
+	public String getOwnerType() {
+		return this.ownerType;
+	}
+	public void setOwnerType(String ownerType) {
+		this.ownerType = ownerType;
+	}
+
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (!(other instanceof AddressPK)) {
+			return false;
+		}
+		AddressPK castOther = (AddressPK)other;
+		return 
+			(this.ownerid == castOther.ownerid)
+			&& this.ownerType.equals(castOther.ownerType);
+	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int hash = 17;
+		hash = hash * prime + this.ownerid;
+		hash = hash * prime + this.ownerType.hashCode();
+		
+		return hash;
+	}
+}
