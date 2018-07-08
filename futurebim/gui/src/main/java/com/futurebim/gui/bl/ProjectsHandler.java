f9ba805ca389f81534f18dba1a8a0782aeeb55a8
tarka <tarka@LAPTOP-JKQ8H0DM.fritz.box>
2018-07-08 16:29:11 +0200

    test

--- futurebim/gui/src/main/java/com/futurebim/gui/bl/ProjectsHandler.java
+++ futurebim/gui/src/main/java/com/futurebim/gui/bl/ProjectsHandler.java
@@ -1,11 +1,7 @@
 package com.futurebim.gui.bl;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
-import com.futurebim.gui.model.futurebim.GuiProjectRich;
 
 
 
@@ -16,18 +12,6 @@ public class ProjectsHandler {
 
 
 
-  public GuiProjectRich getById(final Long id) {
-    return null;
-
-  }
-
-  public List<GuiProjectRich> listProjects(final Long companyId) {
-
-    final List<GuiProjectRich> list = new ArrayList<>();
-
-
-    return list;
-  }
 
 }
 
