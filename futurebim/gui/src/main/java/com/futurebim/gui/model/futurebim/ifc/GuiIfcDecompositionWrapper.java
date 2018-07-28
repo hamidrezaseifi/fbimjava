1546be281181e4389f0a1767a870f166967fe10f
hamid <hamidrezaseifi@gmail.com>
2018-07-28 14:59:20 +0200

    model correctuin

--- futurebim/gui/src/main/java/com/futurebim/gui/model/futurebim/ifc/GuiIfcDecompositionWrapper.java
+++ futurebim/gui/src/main/java/com/futurebim/gui/model/futurebim/ifc/GuiIfcDecompositionWrapper.java
@@ -3,41 +3,50 @@ package com.futurebim.gui.model.futurebim.ifc;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.futurebim.common.model.edo.ifc.IfcProjectEdo;
 import com.futurebim.common.model.edo.ifc.ProjectIfcEdo;
 
 public class GuiIfcDecompositionWrapper {
-  
+
   @JacksonXmlElementWrapper(useWrapping = false)
   @JacksonXmlProperty(localName = "IfcProject")
-  @JsonProperty(value = "IfcProject")
-  private List<GuiIfcProject> children = new ArrayList<>();
-  
-  private final String type = "decomposition";
+  @JsonIgnore
+  private List<GuiIfcProject> projetcs = new ArrayList<>();
 
+  private final String type = "decomposition";
+  
   public GuiIfcDecompositionWrapper(final ProjectIfcEdo edo){
     for(final IfcProjectEdo item: edo.getDecomposition().getProjects()){
-      addChild(new GuiIfcProject(item));
+      addProject(new GuiIfcProject(item));
     }
   }
 
-  public List<GuiIfcProject> getChildren() {
-    return children;
+  public List<GuiIfcProject> getProjetcs() {
+    return projetcs;
   }
   
-  public void setChildren(final List<GuiIfcProject> projects) {
-    this.children = projects;
+  public void setProjetcs(final List<GuiIfcProject> projetcs) {
+    this.projetcs = projetcs;
   }
-  
-  public void addChild(final GuiIfcProject project) {
-    this.children.add(project);
+
+  public void addProject(final GuiIfcProject project) {
+    this.projetcs.add(project);
   }
-  
+
   public String getType() {
     return type;
   }
-  
+
+
+  public List<Object> getChildren() {
+
+    final List<Object> children = new ArrayList<>();
+    
+    children.addAll(projetcs);
+
+    return children;
+  }
 }
