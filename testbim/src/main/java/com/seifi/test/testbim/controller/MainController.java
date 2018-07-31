ac895b13ef415502e21bc3f71d73f865e4a56158
rezasei <rezasei@HAJN72111139.tuid.msds.insite>
2018-07-31 17:27:15 +0200

    start server

--- testbim/src/main/java/com/seifi/test/testbim/controller/MainController.java
+++ testbim/src/main/java/com/seifi/test/testbim/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.seifi.test.testbim.controller;
 
-
+import org.bimserver.BimServer;
+import org.bimserver.BimServerConfig;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(path = "/")
 public class MainController {
+  
   @RequestMapping(path = "/")
   public @ResponseBody String index(final Model model){
     
+    BimServerConfig config = new BimServerConfig();
+    config.setAutoMigrate(true);
+    config.setPort(2020);
+    config.setStartEmbeddedWebServer(true);
+    
+    BimServer serv = new BimServer(config);
+    
     return "test index";
+  }
+  
+  @RequestMapping(path = "/test")
+  public @ResponseBody String test(final Model model){
+    
+    return "test url";
   }
 
 }
